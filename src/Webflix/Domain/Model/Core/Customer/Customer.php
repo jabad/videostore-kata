eed0d9d09a2e09c3f644e036474a2c3201a996c7
Jordi Abad <jordiabad82@gmail.com>
2016-12-21 22:37:13 +0100

    Removing customer unused code

--- src/Webflix/Domain/Model/Core/Customer/Customer.php
+++ src/Webflix/Domain/Model/Core/Customer/Customer.php
@@ -10,75 +10,18 @@ class Customer
     /** @var  string */
     private $name;
 
-    /** @var array  */
-    private $rentals = [];
-
-    /**
-     * Customer constructor.
-     * @param $name
-     */
-    public function __construct($name)
+    private function __construct(string $name)
     {
         $this->name = $name;
     }
 
-    /**
-     * Name accessor.
-     * @return string
-     */
-    public function name() : string
+    public static function instance(string $name)
     {
-        return $this->name;
+        return new static($name);
     }
 
-    /**
-     * @return string
-     */
-    public function statement()
+    public function name(): string
     {
-        /** @var float $totalAmount */
-        $totalAmount = 0;
-        /** @var int $frequentRenterPoints */
-        $frequentRenterPoints = 0;
-        /** @var string $result */
-        $result = "Rental Record for " . $this->name() . "\n";
-
-        /** @var Rental $rental */
-        foreach ($this->rentals as $rental) {
-            $thisAmount = 0;
-
-            switch ($rental->movie()->priceCode()) {
-                case REGULAR:
-                    $thisAmount += 2;
-                    if ($rental->daysRented() > 2) {
-                        $thisAmount += ($rental->daysRented() - 2) * 1.5;
-                    }
-                    break;
-
-                case NEW_RELEASE:
-                    $thisAmount += ($rental->daysRented()) * 3;
-                    break;
-
-                case CHILDRENS:
-                    $thisAmount += 1.5;
-                    if ($rental->daysRented() > 3) {
-                        $thisAmount += ($rental->daysRented() - 3) * 1.5;
-                    }
-                    break;
-            }
-
-            $frequentRenterPoints++;
-            if ($rental->movie()->priceCode() == NEW_RELEASE && $rental->daysRented() > 1) {
-                $frequentRenterPoints++;
-            }
-
-            $result .= "\t" . $rental->movie()->title() . "\t" . $thisAmount . "\n";
-            $totalAmount += $thisAmount;
-        }
-
-        $result .= "You owed " . $totalAmount . "\n";
-        $result .= "You earned " . $frequentRenterPoints . " frequent renter points\n";
-
-        return $result;
+        return $this->name;
     }
 }
